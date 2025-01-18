357defb21e80bb306753f6c8d1aa43af11f9e867
vinsmokejazz <vinsmokejazz@gmail.com>
2025-01-19 00:39:06 +0530

    routers updated

---
+++ routes/todo.js
@@ -0,0 +1,18 @@
+const Router=require("express");
+const todoRouter=Router();
+
+todoRouter.get("/todo", function(req,res){
+
+});
+
+todoRouter.post("/todo", function(req,res){
+
+});
+
+todoRouter.put("/todo", function(req,res){
+
+});
+
+todoRouter.delete("/todo", function(req,res){
+
+});
---
+++ routes/user.js
@@ -0,0 +1,10 @@
+const {Router} = require("express");
+const userRouter = Router();
+
+userRouter.get("/signup", function (req,res){
+
+});
+
+userRouter.get("/signin", function(req,res){
+
+});
